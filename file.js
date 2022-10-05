c80388ff283bcce7abaeca6adee1ba860590c809
Your Name <you@example.com>
2022-10-05 22:13:03 +0530

    Done

---
+++ file.js
@@ -0,0 +1,81 @@
+// ---------Arithmetic operator------------
+
+//Relational operator
+
+
+let number1=20;
+let number2=56;
+console.log(number1<number2);
+console.log(number1<=number2);
+console.log(number1>number2);
+console.log(number1>=number2);
+console.log(number1!=number2);
+
+
+
+//Logical 
+
+
+let Num1=20;
+let Num2=46;
+
+console.log(Num1<10 && Num2>45);
+console.log(Num1==20 || Num2==65);
+console.log(Num1 == Num2);
+
+
+//Assignment
+
+
+let a=10;
+
+// Increment / Decrement 
+
+
+
+let x=10;
+x++;
+console.log(x);
+let y=10;
+y--;
+console.log(y);
+
+
+
+//Conditional
+
+
+let num=40;
+let result= (num>30) ? 'Yes' : 'No';
+console.log(result);
+
+
+
+//Bitwise operator
+
+let num1=4;
+let num2=5;
+console.log(num1 & num2);
+
+let num3=4;
+let num4=5;
+console.log(num1 | num2);
+
+let num5=5;
+let num6=4;
+console.log(num1 ^ num2);
+
+let num7=5;
+let num8=4;
+console.log(num7 << num8);
+
+let num9=5;
+let num10=4;
+console.log(num9 >> num10);
+
+let num11=5;
+console.log( ~ num11);
+
+//type of
+console.log(typeof "John")
+
